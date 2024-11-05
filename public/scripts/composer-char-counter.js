d6430924b805b85e4045e68269afbd9131d0ff7c
Vitor <vi7org@hotmail.com>
2024-11-05 13:31:05 -0500

    make counter show input length, and turn read is it is over the limit

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,13 @@
+﻿$(document).ready(function() {
+    $('.new-tweet textarea').on('input', function() {
+        const text = $(this).val();
+        const counter = $(this).closest('form').find('.counter');
+        if (text.length > 140){
+            counter.text(-(text.length - 140));
+            counter.addClass('error');
+        }else{
+            counter.text(140 - text.length);
+            counter.removeClass('error');
+        }
+    });
+});
